3389814e2259cc311f22faa3b72ca3653b97217a
Anirban <anirbancodes@gmail.com>
2024-04-02 01:33:09 +0530

    added dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log(`Listing on Port: 3000`);
+app.listen(port, () => {
+  console.log(`Listing on Port: ${port}`);
 });
