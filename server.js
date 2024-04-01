1e2116ec542a2ee1380b6d5365202464997fb0fd
Anirban <anirbancodes@gmail.com>
2024-04-02 03:26:01 +0530

    added database

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+const db = "mongodb://localhost:27017/appify";
+mongoose
+  .connect(db, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB Connected Successfully!"));
 
 const port = process.env.PORT || 3000;
 
